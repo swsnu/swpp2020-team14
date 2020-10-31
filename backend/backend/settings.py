b987e8fcb645bd1109f09bd569e6b0f265307f31
Jaewook Lee <jaewooklee@snu.ac.kr>
2020-10-31 19:10:06 +0900

    define Django models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fontopia.apps.FontopiaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
