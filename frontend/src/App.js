22d7d0afadc02ad9945f236e38ed5b38e6c96b9e
Namnamseo <Namnamseo+github@gmail.com>
2020-11-06 06:10:34 +0900

    Add BroswerRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
+import { BrowserRouter, Switch } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div className="navbar">
+        </div>
+
+        <Switch>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
