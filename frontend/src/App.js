270a839a6ddbee763259046b849aae5992faf0f6
Namnamseo <Namnamseo+github@gmail.com>
2020-11-09 07:19:06 +0900

    Add CSRF & create/edit routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 
-import { ArticleDetailView, ArticleListView } from './article/views/all.js';
+import { ArticleDetailView, ArticleListView, ArticleCreateView, ArticleEditView } from './article/views/all.js';
 import { FontListView, FontItemView } from './font/views/all.js';
 
 import './App.css';
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 
 function App() {
   return (
@@ -14,7 +19,9 @@ function App() {
 
         <Switch>
           <Route exact path="/article" component={ArticleListView} />
+          <Route exact path="/article/create" component={ArticleCreateView} />
           <Route exact path="/article/:article_id" component={ArticleDetailView} />
+          <Route exact path="/article/:article_id/edit" component={ArticleEditView} />
           <Route exact path="/font" component={FontListView} />
           <Route exact path="/font/:font_id" component={FontItemView} />
         </Switch>
