df05a4e6b99c3a0e18ae743d7e8c0eef5d3462fd
Jaewook Lee <jaewooklee@snu.ac.kr>
2020-11-14 17:38:04 +0900

    finish sprint 3

---
+++ frontend/src/App.test.js
@@ -0,0 +1,32 @@
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { createBrowserHistory } from 'history';
+import { mount } from 'enzyme';
+
+import loginReducer from './sign/reducers/reducers'
+
+export const history = createBrowserHistory();
+
+describe('App', () => {
+  const reducer = combineReducers({
+    login: loginReducer
+  })
+  const mockStore = createStore(reducer)
+  let app;
+
+  beforeEach(() => {
+    history.location.pathname = "/";
+
+    app = (
+      <Provider store={mockStore}>
+        <App history={history}/>
+      </Provider>
+    )
+  })
+
+  it(`should render without errors`, () => {
+    const component = mount(app);
+    expect(component.find('.App').length).toBe(1);
+  })
+})
