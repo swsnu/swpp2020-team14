eb392a8f962ce43f86e015a2f40dd6fcc06e6dbe
Namnamseo <Namnamseo+github@gmail.com>
2020-12-10 17:12:45 +0900

    Show gaps on page ends

--- frontend/src/common/pagination.js
+++ frontend/src/common/pagination.js
@@ -1,11 +1,22 @@
 import { Button, ButtonGroup } from '@material-ui/core';
 import React, { Component } from 'react';
 
+const around = 4;
+
 class PageButtonArray extends Component {
   render() {
     const arr = [];
     const H = (i) => ((e) => { this.props.onclick(i); e.preventDefault(); });
-    for (let i = this.props.cur - 5; i <= this.props.cur + 5; ++i) {
+
+    if (1 < this.props.cur - around) {
+      arr.push(
+        <Button key="right-gap" className="page-btn" variant={ "outlined" } disabled>
+          …
+        </Button>
+      );
+    }
+
+    for (let i = this.props.cur - around; i <= this.props.cur + around; ++i) {
       if (i < 1 || i > this.props.n) continue;
       arr.push(
         <Button
@@ -17,6 +28,15 @@ class PageButtonArray extends Component {
           {i}
         </Button>);
     }
+
+    if (this.props.cur + around < this.props.n) {
+      arr.push(
+        <Button key="right-gap" className="page-btn" variant={ "outlined" } disabled>
+          …
+        </Button>
+      );
+    }
+
     return (
       <div className="page-button-array">
         <ButtonGroup color="primary">
